2b6f45d4ff408745e9099064eb9e19232cd2d13c
Shibani987 <sd773064@gmail.com>
2026-05-13 16:38:14 +0530

    GhostFix AI v0.2.0 release

--- tests/real_world_failures/pandas_missing_column.py
+++ tests/real_world_failures/pandas_missing_column.py
@@ -1,6 +1,5 @@
 orders = {
-    # Simulating pandas DataFrame access
-    "order_id": [101, 102],
+    "order_id": [101, 102],  # Simulating pandas DataFrame access
     "total": [24.50, 13.25],
 }
 
