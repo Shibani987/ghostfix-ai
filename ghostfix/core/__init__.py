a35962eb25df79d16cf455db3665815485d0d92c
Shibani987 <sd773064@gmail.com>
2026-06-06 14:09:10 +0530

    Fix CLI imports and pass build pipeline

--- ghostfix/core/__init__.py
+++ ghostfix/core/__init__.py
@@ -1,6 +1,5 @@
-from .watcher import ProcessWatcher
 from .error_parser import ErrorParser, ParsedError
 from .context_builder import ContextBuilder, CodeContext
 from .patcher import Patcher
 
-__all__ = ["ProcessWatcher", "ErrorParser", "ParsedError", "ContextBuilder", "CodeContext", "Patcher"]
+__all__ = ["ErrorParser", "ParsedError", "ContextBuilder", "CodeContext", "Patcher"]
